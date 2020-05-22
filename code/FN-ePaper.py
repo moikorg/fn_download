33fa1bb52738d268828e07bb19eeeedb8b5f6824
Michael Mäder <michael.maeder@hefr.ch>
2020-05-22 11:08:26 +0200

    add mail sending

--- code/FN-ePaper.py
+++ code/FN-ePaper.py
@@ -3,25 +3,42 @@ import sys, os
 from datetime import datetime
 import configparser
 import smtplib
+import ssl
 from email.message import EmailMessage
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email import encoders
 
 
-def sendMail(cfg, to_addr, msg):
-    from_addr = "michael.maeder@hefr.ch"
+def sendMail(cfg, to_addr, msg, atta):
+    from_addr = "no-reply@moik.org"
 
-    server = smtplib.SMTP(host=cfg['SMTP']['server'],
-                          port=cfg['SMTP']['port'])
-    server.set_debuglevel(1)
+    context = ssl.create_default_context()
+    server = smtplib.SMTP(host=cfg['SMTP_server'],
+                          port=cfg['SMTP_port'])
+    # server.set_debuglevel(1)
+    server.ehlo()
+    server.starttls(context=context)
+    server.ehlo()
+    server.login(cfg['SMTP_user'], cfg['SMTP_pw'])
 
     # create the message
-    email_message = EmailMessage()
-    email_message.set_content(msg)
-    email_message['Subject'] = "Freiburger Nachrichten"
-    email_message['From'] = from_addr
-    email_message['To'] = to_addr
+    message = MIMEMultipart()
+    message['Subject'] = "Freiburger Nachrichten"
+    message['From'] = from_addr
+    message['To'] = to_addr
+    message.attach(MIMEText(msg, 'plain'))
 
-    server.send_message(email_message)
-#    server.sendmail(from_addr, to_addr, msg.encode('ascii', 'ignore'))
+    attachement = MIMEBase("application", "octet-stream")
+    attachement.set_payload(atta)
+    encoders.encode_base64(attachement)
+    pdfFilename = "FN_" + datetime.now().strftime('%Y%m%d') + ".pdf"
+    attachement.add_header("Content-Disposition", f"attachement; filename={pdfFilename}")
+    message.attach(attachement)
+    text = message.as_string()
+    server.sendmail(from_addr, to_addr, text)
+
     server.quit()
 
 
@@ -39,6 +56,11 @@ def getConfig():
         'fn_pass':config['FN-Credentials']['password'],
         'nc_user': config['Nextcloud-Credentials']['username'],
         'nc_pass': config['Nextcloud-Credentials']['password'],
+        'SMTP_server': config['SMTP']['server'],
+        'SMTP_port': config['SMTP']['port'],
+        'SMTP_user': config['SMTP']['username'],
+        'SMTP_pw': config['SMTP']['password'],
+        'recepient': config['RECEPIENT']['addr']
     }
     return credentials
 
@@ -79,10 +101,14 @@ def main():
     if r.status_code != 200:
         return 1
 
-    # write the file to the filesystem... this is for debugging
+    # # write the file to the filesystem... this is for debugging
     # f = open('./out.pdf', 'wb')
     # f.write(r.content)
     # f.close()
+
+    mailMsg = "Hallo, hier die FN von heute. Gruss"
+    addr = credentials['recepient']
+    sendMail(credentials, addr, mailMsg, r.content)
 
     # write the document to nextcloud
     try:
@@ -93,6 +119,7 @@ def main():
     except requests.exceptions.ConnectionError as err:
         print(err)
         return 1
+
     return 0
 
 
